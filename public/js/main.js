60c386de498faf16f4e4c35c7a791653f8aab813
kleveland <kacey.job@gmail.com>
2017-12-22 18:25:27 -0500

    added basic user login

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,51 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		anchors: ['home', 'about', 'cv', 'projects'],
-        afterLoad: function(anchorLink, index) {
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors: ['home', 'about', 'cv', 'projects'],
+        afterLoad: function (anchorLink, index) {
             $('.navbar > ul > li').css('opacity', 1);
             $('.navbar > ul > li').css('color', invertColor($('#fullpage .section:nth-child(' + index + ') .mainpanel').css('backgroundColor')));
+            $('.navbar > ul > li:nth-child(' + index + ')').addClass('navhover');
         },
-        onLeave: function(index, nextIndex, direction) {
+        onLeave: function (index, nextIndex, direction) {
+            $('.navbar > ul > li:nth-child(' + index + ')').removeClass('navhover');
+            $('.navbar > ul > li:nth-child(' + nextIndex + ')').addClass('navhover');
             $('.navbar > ul > li').css('color', invertColor($('#fullpage .section:nth-child(' + nextIndex + ') .mainpanel').css('backgroundColor')));
         }
-	});
+    });
+
+    typeOut('.mainText', 'Kacey Cleveland', () => {
+        let mainT = $('.mainText').text();
+        setInterval(() => {
+            console.log(mainT);
+            if (mainT[mainT.length - 1] == '_') {
+                $('.mainText').text(mainT.replace('_', ''));
+            } else {
+                $('.mainText').text(mainT + '_');
+            }
+            mainT = $('.mainText').text();
+        }, 600);
+    })
 
 });
 
 function invertColor(rgb) {
-  rgb = Array.prototype.join.call(arguments).match(/(-?[0-9\.]+)/g);
-  for (var i = 0; i < rgb.length; i++) {
-    rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
-  }
-  return 'rgb(' + rgb[0] +',' + rgb[1] + ',' + rgb[2] + ')';
+    rgb = Array.prototype.join.call(arguments).match(/(-?[0-9\.]+)/g);
+    for (var i = 0; i < rgb.length; i++) {
+        rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
+    }
+    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+}
+
+function typeOut(el, txt, cb) {
+    for (let i = 0; i < txt.length; i++) {
+        ((i) => {
+            setTimeout(() => {
+                console.log($(el).text());
+                $(el).append(txt[i]);
+            }, 300*i)
+        })(i);
+    }
+    setTimeout(() => {
+        cb();
+    }, 300*txt.length);
 }
--- serv.js
+++ serv.js
@@ -1,6 +1,43 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const config = require('./config.json')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const session = require('express-session')
+//passport setup
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.auth.google.client_id,
+        clientSecret: config.auth.google.secret,
+        callbackURL: config.auth.google.callback
+    },
+    function (accessToken, refreshToken, profile, done) {
+        /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
+          return done(err, user);
+        });*/
+        console.log("connected", JSON.stringify(profile))
+        return done(null, profile);
+    }
+));
+
+app.use(session({
+    secret: 'cookie_secret',
+    name: 'kaas',
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.set('views', __dirname + '/views');
@@ -8,8 +45,33 @@ app.set('view engine', 'pug');
 
 console.log(path.join(__dirname, 'public'));
 
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['email', 'profile']
+    }));
+
 app.get('/', (req, res) => {
-    res.render('home', { title: "Title Message", nav: ["Home", "About Me", "Skills and CV", "Projects"], navLink: ['home', 'about', 'cv', 'projects']});
+    console.log(req.session)
+    res.render('home', {
+        title: "Title Message",
+        nav: ["Home", "About Me", "Skills and CV", "Projects"],
+        navLink: ['home', 'about', 'cv', 'projects'],
+        user: req.session
+    });
 })
+
+app.get('/admin', (req, res) => {
+    res.render('admin', {
+        title: "Title Message",
+        nav: ["Home", "About Me", "Skills and CV", "Projects"],
+        navLink: ['home', 'about', 'cv', 'projects']
+    });
+})
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/admin'
+    }));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
